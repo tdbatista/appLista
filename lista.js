d824c13351ba02ab832b1e1a7fee6d3402f76365
tdbatista <tdbatista@hotmail.com>
2019-11-19 12:19:38 -0300

    Mudanças
    Foi adicionado função renderlista, e utilização de JSON para salvar a lista

--- lista.js
+++ lista.js
@@ -1,28 +1,63 @@
 var lista=document.getElementById("lista");
+var item=document.querySelector("ul#lista li");
+var link=document.querySelector("ul#lista li a");
+var button=document.querySelector("ul#lista li button");
+
+if (localStorage.getItem("lista")==null)
+    var todo = new Array(0);
+else
+var todo = JSON.parse(localStorage.getItem("lista")); 
+
+console.log(item,link,button);
+console.log(localStorage.getItem("lista"));
+console.log(todo.length);
+renderlista();
 
 function excluirlista(i){
+    todo.splice(i,1);   
+    renderlista();
+}
 
-    var itens=lista.childNodes;
-    console.log(itens);
-    var item=document.getElementById(i);
-    lista.removeChild(item);
+function salvalista(){
+    localStorage.setItem("lista",JSON.stringify(todo));
+
+}
+
+function renderlista(){
+    lista.innerHTML='';
+    for(todoItem of todo){
+        if (todoItem!=undefined){
+            var newItem = document.createElement("li");
+            var newLink = document.createElement("a");
+            var newButton = document.createElement("button");    
+            newLink.setAttribute("href","http://"+todoItem[1]);
+            newLink.setAttribute("target","_blank");
+            newLink.appendChild(document.createTextNode(todoItem[0]));
+            newButton.appendChild(document.createTextNode("Excluir"));
+            newButton.setAttribute("onclick","excluirlista(" + todo.indexOf(todoItem)+")");
+            newItem.appendChild(newLink);
+            newItem.appendChild(newButton);
+            lista.appendChild(newItem);
+        }
+    }
+
+    var newItem = document.createElement("li");
+    newItem.setAttribute("id","novo");
+    var newButton = document.createElement("button");
+    newButton.appendChild(document.createTextNode("Adicionar"));
+    newButton.setAttribute("onclick","addlista()");
+    newButton.setAttribute("id","add");
+    //var liadd = document.querySelector("ul#lista li#novo");
+    newItem.appendChild(newButton);        
+    lista.appendChild(newItem);
+    salvalista();
 }
 
 function addlista(){
     var diag =  document.getElementById("adddiag");
     var elementAdd = document.getElementById("novo");
     elementAdd.style.visibility="hidden";
-    var elementNome = document.createElement("input");
-    elementNome.setAttribute("id","nome");
-    var elementLink = document.createElement("input");
-    elementLink.setAttribute("id","link");    
-    var elementOk = document.createElement("button");
-    elementOk.setAttribute("onclick","confirmaadd()");    
-    var textBtn = document.createTextNode("OK");
-    elementOk.appendChild(textBtn);
-    diag.appendChild(elementNome);
-    diag.appendChild(elementLink);
-    diag.appendChild(elementOk);
+    diag.style.visibility="visible";
 }
 
 function tamlista(){
@@ -34,24 +69,12 @@ function confirmaadd(){
     var elementAdd = document.getElementById("novo");
     var elementNome = document.getElementById("nome");
     var elementLink = document.getElementById("link");
-   
-    var item=document.createElement("li");
-    var link=document.createElement("a");
-    var btn = document.createElement("button");
-    item.setAttribute("id",tamlista()-1);
-    link.appendChild(document.createTextNode(elementNome.value));
-    link.setAttribute("href","http://"+elementLink.value);
-    link.setAttribute("target","_blank");
-    btn.appendChild(document.createTextNode("Excluir"));
-    btn.setAttribute("id","excluir");
-    btn.setAttribute("onclick","excluirlista("+(tamlista()-1)+")");
-    item.appendChild(link);
-    item.appendChild(btn);
-    elementAdd.remove;
-    lista.appendChild(item);
-    lista.appendChild(elementAdd);
+    todo.push([elementNome.value,elementLink.value]);
     elementAdd.style.visibility="visible"; 
     var diag =  document.getElementById("adddiag");
-    while (diag.firstChild)diag.removeChild(diag.firstChild); 
+    diag.style.visibility="hidden";
+    elementNome.value='';
+    elementLink.value='';
+    renderlista();
     
 }
